8f91be198af270d8b6dba40af08a3f648a2b25c0
Jacob <saucyeti@ur-mom.localdomain>
2025-09-24 23:17:50 -0700

    Add simple UDP server implementation

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # UDP-server
-A simple UDP server.
+A simple UDP server. I made this mainly to learn C++ socket programming.
+
+## C++ Networking Library (or rather a lack thereof)
+
+C++ does not include built-in networking capabilities in its standard library. As such, there is no standard way to do socket programming.
+
+### POSIX's networking related APIs
+
+POSIX stands for Portable Operating System Interface (for uniX) and is a famiily of standards to maintain compatibility between operating systems. This makes it eaiser to write software for these different Unix operating systems. Though not originally part of it, the Berkeley Sockets API was later incorporated into the POSIX standards. The POSIX standards are primarily written as C function specifications, meaning C and C++ code can directly call POSIX functions. This allows us to use the Berkeley Sockets API in our C++ code to make system calls to the kernel to send and receive data. 
+
+### ASIO
+
+ASIO (ASynchronous Input Output) is a cross platform C++ library that provides higher level abstractions for networking. Some of its main selling points are that it works well with modern C++ (co-routines, stronger type safety), can be asynchronous (as the name suggests), cross platform, and inspired the C++ networking TS. Most of the time you would probably want to choose ASIO over POSIX, except in performance critical scenarios where you can't afford the abstraction overhead (though at that point one might consider writing their own custom network stack or kernel bypass).
+
+## Sockets
+
+### High Level
+
+At a high level, a socket is an endpoint for network communication. Think of it like a telephone where you can call/connect to another telephone. There are different types of sockets for different purposes (for example TCP/UDP).
+
+### Low Level
+
+Technically, sockets are just an abstraction provided by the OS. A socket API like Berkeley Sockets API provides an interface for us to make system calls to the kernel. The socket itself is just a data structure in kernel memory. When we create a socket, the kernel returns a file descriptor that points to an entry in that process's file descriptor table which points to a special inode that always points to memory (instead of disk space). This memory contains the data sturcture(s) that house the buffers for network communication.
---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <netinet/in.h> // For sockaddr_in, IPPROTO_UDP, IDNADDR_ANY, htons, etc.
+#include <string>
+#include <sys/socket.h> // For socket functions
+#include <unistd.h> // For close()
+
+int main() {
+    int sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    if (sock_fd < 0) {
+        std::cerr << "Socket creation failed" << std::endl;
+        return -1;
+    }
+
+    /*
+    Initialize to zero as we don't set sin_zero explicitly
+    sockaddr_in has a sin_zero field to pad the struct to 16 bytes (size of sockaddr)
+    sockaddr is a generic address struct for all types of sockets, so we need to cast sockaddr_in to sockaddr
+    */
+    sockaddr_in server_addr {};
+
+    server_addr.sin_family = AF_INET; // IPv4
+    server_addr.sin_addr.s_addr = INADDR_ANY; // Accept connections from any IP address
+    server_addr.sin_port = htons(8080); // htons converts to network byte order (big-endian)
+
+    /*
+    Note that we use reinterpret_cast to cast sockaddr_in* to sockaddr*
+    Even though this is unsafe, it is required since bind expects a sockaddr*
+    Static cast would not work here as sockaddr_in and sockaddr do not share a parent-child relationship
+    */
+    if (bind(sock_fd, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr)) < 0) {
+        std::cerr << "Bind failed" << std::endl;
+        close(sock_fd);
+        return -1;
+    }
+
+    std::cout << "Server is listening on port 8080" << std::endl;
+
+    while (true) {
+        char buffer[1024] {};
+        sockaddr_in client_addr {};
+        socklen_t client_len = sizeof(client_addr);
+
+        ssize_t bytes_read = recvfrom(sock_fd, buffer, sizeof(buffer) - 1, 0, 
+            reinterpret_cast<sockaddr*>(&client_addr), &client_len);
+
+        if (bytes_read < 0) {
+            std::cerr << "Error receiving data" << std::endl;
+            continue;
+        }
+
+        buffer[bytes_read] = '\0'; // Null terminate
+        std::cout << "Received: " << buffer << std::endl;
+
+        std::string response = "Echo: " + std::string(buffer);
+        sendto(sock_fd, response.c_str(), response.size(), 0,
+            reinterpret_cast<sockaddr*>(&client_addr), client_len);
+    }
+
+    /*
+    Close releases the file descriptor number back to the OS
+    It unbinds the port and frees kernel memory associated with the socket
+    */
+    close(sock_fd);
+    return 0;
+};
